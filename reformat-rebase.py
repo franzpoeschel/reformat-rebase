9e1d955af6bcbfbdbd60995852d748c21b821f63
Franz Pöschel <franz.poeschel@gmail.com>
2021-11-09 17:31:30 +0100

    Fix issue with losing commit information

--- reformat-rebase.py
+++ reformat-rebase.py
@@ -12,9 +12,6 @@ def parse_args(program_name):
         description="todo: add description")
 
     parser.add_argument('--old-base', type=str, help='Old git base commit.')
-    parser.add_argument('--new-base',
-                        type=str,
-                        help='New git (re)base commit.')
     parser.add_argument('--new-branch', type=str, help="New branch name"),
     parser.add_argument('--reformat',
                         type=str,
@@ -75,21 +72,36 @@ class GitHelpers:
             GitHelpers.steal_and_reapply_commit(commit, reformat)
 
 
-def main(old_base, new_base, new_branch, reformat):
+def main(old_base, new_branch, reformat):
     commit_history = GitHelpers.commits_from_to(old_base,
                                                 "HEAD",
                                                 from_inclusive=False)
-    checkout_command = ["git", "checkout", new_base]
+    checkout_command = ["git", "checkout", old_base]
     GuardSyscall.call(*checkout_command)
     new_branch_command = ["git", "checkout", "-b", new_branch]
     GuardSyscall.call(*new_branch_command)
+    GuardSyscall.call(*reformat)
+    commit_command = [
+        "git", "commit", ".",
+        "--message=Temporary commit for reformat. Skip when rebasing."
+    ]
+    GuardSyscall.call(*commit_command)
+    get_current_commit_call = "git rev-list HEAD^..HEAD"
+    temp_commit = GuardSyscall.getoutput(get_current_commit_call)
     GitHelpers.reapply_commit_range(commit_history, reformat)
+    print("""
+You can now rebase onto the commit that reformatted your code base:
+> git rebase --onto <new_base> {}
+
+Not doing that automatically as rebasing might fail and you need
+to fix any issues manually.
+""".format(temp_commit))
 
 
 if __name__ == "__main__":
     args = parse_args(sys.argv[0])
-    if not args.reformat or not args.old_base or not args.new_base or not args.new_branch:
+    if not args.reformat or not args.old_base or not args.new_branch:
         raise Exception("All arguments must be specified.")
 
-    main(args.old_base, args.new_base, args.new_branch, args.reformat)
+    main(args.old_base, args.new_branch, args.reformat)
     sys.exit()
